1900cced827a860b51269a03f8ab6f6f073f41e1
joearrigo <joeyma917@gmail.com>
2018-05-02 17:12:37 -0400

    From Windows side. Update

---
+++ ControlPanel/src/controlpanel/ControlPanel.java
@@ -0,0 +1,32 @@
+package controlpanel;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Joseph Arrigo
+ */
+public class ControlPanel extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
